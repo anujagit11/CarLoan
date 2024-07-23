2fe5f08fddfa0855815594682ea8748c56138e55
anujagit11 <saykhede29@gmail.com>
2024-07-23 10:50:09 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './templates/home/home.component';
+import { VisitorsComponent } from './templates/visitors/visitors.component';
+
+import { VisitorHeadersComponent } from './includes/visitor-headers/visitor-headers.component';
+import { SignInComponent } from './includes/sign-in/sign-in.component';
+import { UserDashboardComponent } from './includes/user-dashboard/user-dashboard.component';
+import { EmiCalculatorsComponent } from './includes/emi-calculators/emi-calculators.component';
+import { EnquiryComponent } from './includes/enquiry/enquiry.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserHeaderComponent } from './includes/user-header/user-header.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    VisitorsComponent,
+    
+    VisitorHeadersComponent,
+    SignInComponent,
+    UserDashboardComponent,
+    EmiCalculatorsComponent,
+    EnquiryComponent,
+    UserHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,ReactiveFormsModule,HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/includes/emi-calculators/emi-calculators.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emi-calculators',
+  templateUrl: './emi-calculators.component.html',
+  styleUrls: ['./emi-calculators.component.css']
+})
+export class EmiCalculatorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
